ae664c01fdece446d2b109f764d73f6743f1dedb
Anvithareddy4 <118528372+Anvithareddy4@users.noreply.github.com>
2023-02-01 22:19:53 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,7 @@
 function run()
 {
+  /*This is multi line comment
+  used in javascript*/
   let htmlcode=document.querySelector(".container #html-code").value;
   let csscode="<style>"+document.querySelector(".container #css-code").value+"<style>";
   let jscode=document.querySelector(".container #js-code").value;
