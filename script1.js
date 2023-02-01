dc96500d518a2ee8e104371270a6439bbac46671
Anvithareddy4 <118528372+Anvithareddy4@users.noreply.github.com>
2023-02-01 22:24:14 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,7 @@
 function run()
 {
+  /*It is an example
+  to compare branches*/
   let htmlcode=document.querySelector(".container #html-code").value;
   let csscode="<style>"+document.querySelector(".container #css-code").value+"<style>";
   let jscode=document.querySelector(".container #js-code").value;
